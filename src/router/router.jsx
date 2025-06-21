20d2b5abcbe996b8cbbc444155c32a1481349dd3
iTsRH98 <rhnayemfardin@gmail.com>
2025-06-21 10:38:32 +0600

    added register page, login page, privateRoute and others functionality fot authenticate user

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
+import Register from "../pages/Authentication/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +13,20 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
       },
     ],
   },
